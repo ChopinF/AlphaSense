cfc428f1face00e30859fb57f35a94c0647e4e33
ChopinF69 <lazaralexandru14@yahoo.com>
2025-03-05 15:48:45 +0200

    added analytics - fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen antialiased">
         <Navbar />
         <main className="flex-grow">{children}</main>
+        <Analytics />
         <footer>
           <Footer />
         </footer>
