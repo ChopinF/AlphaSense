684f58af083681be914607fbba5c229f73fcb3be
ChopinF69 <lazaralexandru14@yahoo.com>
2025-03-05 17:40:23 +0200

    Fixed bugs v2

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -54,8 +54,9 @@ export default function Navbar() {
             <li className="relative">
               <button
                 onClick={() => {
-                  setIsQuizOpen(!isQuizOpen),
-                    setIsHearingOpen(false);
+                  setIsQuizOpen(!isQuizOpen);
+                  if (isHearingOpen)
+                    setIsHearingOpen(!isHearingOpen);
                 }}
                 className="flex items-center py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white"
               >
